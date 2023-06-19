0e20351f6f1471571cee9250bdbbc3184c932f07
Andrea Hrelja <andhrelja@hotmail.com>
2023-06-19 17:50:19 +0200

    feat: streamline resume creation
    BREAKING CHANGE: Update version to 1.0.0

--- README.md
+++ README.md
@@ -1,3 +1,32 @@
 # Resume Builder
 
 Sourced from: https://github.com/brndnmtthws/resume
+
+## Overview
+
+- `assets/`
+  - contains static data used by `resume.tex`
+- `components/`
+  - styles resume components
+  - mapped to `resume.toml` dictionary keys
+- `resume_builder/`
+  - Python package that:
+    - iterates over `resume.toml` dictionary keys
+    - reads the mapped `components` and formats the in-memory strings using `resume.toml` values (`str.format` function)
+- `resume.toml`
+  - each dictionary key contains a corresponding `component`
+  - the keys inside nested dictionaries represent `component` values to be formatted by `resume-builder`
+
+## Usage
+
+```shell
+git clone https://github.com/andhrelja/resume-builder
+cd resume-builder
+pip install .
+```
+
+Update `resume.toml` with desired values. If adding new dictionary keys, make sure to create a corresponding `components/<component>.pytex` file and add the dictionary keys to `components/main.pytex`.
+
+## Feedback
+
+GitHub Issues: https://github.com/andhrelja/resume-builder/issues
--- resume_builder/resume_builder.py
+++ resume_builder/resume_builder.py
@@ -1,1 +1,59 @@
+import json
+import logging
+from pathlib import Path
 
+from . import utils
+
+logger = logging.getLogger('resume_builder')
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+COMPONENTS_DIR = BASE_DIR / 'components'
+MAIN_COMPONENT = COMPONENTS_DIR / 'main.pytex'
+
+RESUME_TOML = BASE_DIR / 'resume.toml'
+RESUME_TEX = BASE_DIR / 'resume.tex'
+
+
+def format_professional_experience(professional_experiences, pytex):
+    pytexs = []
+    for exp in professional_experiences:
+        project_involvement = r'\item ' + r'\item '.join(map(lambda x: x + '\n\t\t', exp['project_involvement']))
+        exp['project_involvement'] = project_involvement
+        pytexs.append(pytex.format(**exp))
+    return pytexs
+
+
+def format_languages(languages, content):
+    languages = '\n\t'.join(("{}: {}".format(lang, level) for lang, level in languages.items()))
+    return content.format(languages=languages)
+
+
+def populate_contents(resume_config):
+    contents = resume_config.pop('author')
+    for key, kwargs in resume_config.items():
+        with open(COMPONENTS_DIR / (key + '.pytex'), 'r') as f:
+            content = f.read()
+        if key == 'professional_experience':
+            contents[key] = '\n'.join(format_professional_experience(kwargs, content))
+        elif key == 'languages':
+            contents[key] = format_languages(kwargs, content)
+        else:
+            contents[key] = content.format(**kwargs)
+    return contents
+
+
+def resume_builder():
+    resume_config = utils.read_toml(RESUME_TOML)
+    main_component = utils.read_text(MAIN_COMPONENT)
+    
+    logger.info("Resume config: %s", json.dumps(resume_config, indent=2))
+    
+    contents = populate_contents(resume_config)
+    resume_content = main_component.format(**contents)
+    
+    utils.write_text(RESUME_TEX, resume_content, encoding='utf-8')
+    logger.info("Succesfully exported %s", RESUME_TEX)
+
+
+if __name__ == '__main__':
+    resume_builder()
